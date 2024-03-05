008acf0adf73515d9d411c32dc17b9f271eba64c
Muhammed Nishad c <122031511+mhdnishadc@users.noreply.github.com>
2024-03-05 11:18:47 +0530

    counter v1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './Components/Counter'
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-center mt-5'>Counter App</h1>
+    <div style={{height:"70vh"}} className="d-flex justify-content-center">
+    <Counter/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
